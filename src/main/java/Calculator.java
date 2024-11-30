a28a3d0b56c5d42e841c2cbdf9cf5537e3361916
Michael <michaelkivo977@gmail.com>
2024-11-30 22:18:32 +0300

    implement calculator logic and update README.md

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,23 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public static void main(String[] args) {}
+
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        return b != 0 ? a / b : null;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        return dif(times(add(2, 3), 2), div(36, 6));
     }
 }
